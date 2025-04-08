0e33908e48305cb35efdd4ca436dba9b413decc9
dvarotech <david.vaughan@aro.tech>
2025-04-08 15:46:50 +0100

    Update StartOSD.ps1

--- StartOSD.ps1
+++ StartOSD.ps1
@@ -1,10 +1,4 @@
-@ECHO OFF
-wpeinit
-cd\
-title OSD 25.1.26.3
 PowerShell -Nol -C Initialize-OSDCloudStartnet
 PowerShell -Nol -C Initialize-OSDCloudStartnetUpdate
-@ECHO OFF
-ECHO Start-OSDCloud
-start /wait PowerShell -NoL -C Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Pro -OSLanguage en-GB -OSActivation Retail -ZTI -Restart
-@ECHO ON
+PowerShell -NoL -C Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Pro -OSLanguage en-GB -OSActivation Retail -ZTI -Restart
+
