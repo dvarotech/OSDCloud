61a0b591404607b5ae5d89dbcc25541d6a4cc617
dvarotech <david.vaughan@aro.tech>
2025-04-08 17:36:31 +0100

    Update StartOSD.ps1

--- StartOSD.ps1
+++ StartOSD.ps1
@@ -1,4 +1,2 @@
-Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
-Install-Module -Name WriteAscii
-Powershell is fun!’ | Write-Ascii -ForegroundColor Rainbow -Compress
+Write-Host "Starting ARO Internal IT Windows 11 24h2." -ForegroundColor Green
 Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Pro -OSLanguage en-gb -OSActivation Retail -Firmware -ZTI -Restart
