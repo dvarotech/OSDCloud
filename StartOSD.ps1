130fc5be92520503fa68eb137119efc27eb62816
dvarotech <david.vaughan@aro.tech>
2025-04-08 15:43:19 +0100

    Update StartOSD.ps1

--- StartOSD.ps1
+++ StartOSD.ps1
@@ -1,3 +1,10 @@
-Set-ExecutionPolicy RemoteSigned -Force
-Install-Module OSD -Force
-StartOSDCloud "-OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Pro -OSLanguage de-de -OSLicense Retail"
+@ECHO OFF
+wpeinit
+cd\
+title OSD 25.1.26.3
+PowerShell -Nol -C Initialize-OSDCloudStartnet
+PowerShell -Nol -C Initialize-OSDCloudStartnetUpdate
+@ECHO OFF
+ECHO Start-OSDCloud
+start /wait PowerShell -NoL -C Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Pro -OSLanguage en-GB -OSActivation Retail -ZTI -Restart
+@ECHO ON
