6e28b41aabdce1e56b259cb440b329470dcfa362
dvarotech <david.vaughan@aro.tech>
2025-04-08 15:59:03 +0100

    Update StartOSD.ps1

--- StartOSD.ps1
+++ StartOSD.ps1
@@ -1,6 +1,1 @@
-PowerShell -Nol -C Initialize-OSDCloudStartnet
-PowerShell -Nol -C Initialize-OSDCloudStartnetUpdate
-PowerShell -NoL -C Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Pro -OSLanguage en-GB -OSActivation Retail -ZTI -Restart
-
-
-Start-sleep 300
+Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Pro -OSLanguage en-gb -OSActivation Retail -Firmware -ZTI -Restart
