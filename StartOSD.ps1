aa65c55c0ea574256c18b55c5d9a6695a64976b5
dvarotech <david.vaughan@aro.tech>
2025-04-08 17:22:36 +0100

    Update StartOSD.ps1

--- StartOSD.ps1
+++ StartOSD.ps1
@@ -1,5 +1,5 @@
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 Install-Module -Name WriteAscii
-
 Powershell is fun!’ | Write-Ascii -ForegroundColor Rainbow -Compress
 
 Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Pro -OSLanguage en-gb -OSActivation Retail -Firmware -ZTI -Restart
