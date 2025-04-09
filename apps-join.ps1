ad37d8a12934cd1c8c44ca017a0f1e8f248caee4
dvarotech <david.vaughan@aro.tech>
2025-04-09 11:09:27 +0100

    Create apps-join.ps1

---
+++ apps-join.ps1
@@ -0,0 +1,19 @@
+(New-Object System.Net.WebClient).DownloadFile("https://merlot.centrastage.net/csm/profile/downloadAgent/513d13b1-2a61-460f-8f2a-730c64acb7c4", "$env:TEMP/AgentInstall.exe");start-process "$env:TEMP/AgentInstall.exe"
+
+
+winget install -e --silent --accept-source-agreements --accept-package-agreements Dell.CommandUpdate
+winget install -e --silent --accept-source-agreements --accept-package-agreements Microsoft.Teams
+winget install -e --silent --accept-source-agreements --accept-package-agreements Microsoft.Edge
+winget install -e --silent --accept-source-agreements --accept-package-agreements Google.Chrome
+winget install -e --silent --accept-source-agreements --accept-package-agreements Mozilla.Firefox
+winget install -e --silent --accept-source-agreements --accept-package-agreements Microsoft.Teams
+winget install -e --silent --accept-source-agreements --accept-package-agreements Adobe.Acrobat.Reader.64-bit
+winget install -e --silent --accept-source-agreements --accept-package-agreements 7zip.7zip
+winget install -e --silent --accept-source-agreements --accept-package-agreements WatchGuard.MobileVPNWithSSLClient
+winget install -e --silent --accept-source-agreements --accept-package-agreements VideoLAN.VLC
+winget install -e --silent --accept-source-agreements --accept-package-agreements Microsoft.OneDrive
+winget install -e --silent --accept-source-agreements --accept-package-agreements Microsoft.Office
+
+
+Rename-Computer -NewName (Read-Host "Enter the new computer name") -Force
+Add-Computer -DomainName arrow.local -Server arrow\core-dc-02 -PassThru -Verbose
