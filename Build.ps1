921c84036ba37bd5eb7bfa1ef8a6de7902627275
dvarotech <david.vaughan@aro.tech>
2025-04-09 11:07:42 +0100

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,18 +1,18 @@
 (New-Object System.Net.WebClient).DownloadFile("https://merlot.centrastage.net/csm/profile/downloadAgent/513d13b1-2a61-460f-8f2a-730c64acb7c4", "$env:TEMP/AgentInstall.exe");start-process "$env:TEMP/AgentInstall.exe"
 
 
-winget -e --silent --accept-source-agreements --accept-package-agreements install Dell.CommandUpdate
-winget install Microsoft.Teams
-winget install Microsoft.Edge
-winget install Google.Chrome
-winget install Mozilla.Firefox
-winget install Microsoft.Teams
-winget install Adobe.Acrobat.Reader.64-bit
-winget install 7zip.7zip
-winget install WatchGuard.MobileVPNWithSSLClient
-winget install VideoLAN.VLC
-winget install Microsoft.OneDrive
-winget install Microsoft.Office
+winget install -e --silent --accept-source-agreements --accept-package-agreements Dell.CommandUpdate
+winget install -e --silent --accept-source-agreements --accept-package-agreements  Microsoft.Teams
+winget install -e --silent --accept-source-agreements --accept-package-agreements  Microsoft.Edge
+winget install -e --silent --accept-source-agreements --accept-package-agreements  Google.Chrome
+winget install -e --silent --accept-source-agreements --accept-package-agreements  Mozilla.Firefox
+winget install -e --silent --accept-source-agreements --accept-package-agreements  Microsoft.Teams
+winget install -e --silent --accept-source-agreements --accept-package-agreements  Adobe.Acrobat.Reader.64-bit
+winget install -e --silent --accept-source-agreements --accept-package-agreements  7zip.7zip
+winget install -e --silent --accept-source-agreements --accept-package-agreements  WatchGuard.MobileVPNWithSSLClient
+winget install -e --silent --accept-source-agreements --accept-package-agreements  VideoLAN.VLC
+winget install -e --silent --accept-source-agreements --accept-package-agreements  Microsoft.OneDrive
+winget install -e --silent --accept-source-agreements --accept-package-agreements  Microsoft.Office
 
 
 Rename-Computer -NewName (Read-Host "Enter the new computer name") -Force
