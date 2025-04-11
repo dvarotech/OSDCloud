26abe207ea57c1f51a35b38ebf2e7f6d9eaaaeed
dvarotech <david.vaughan@aro.tech>
2025-04-11 14:25:14 +0100

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,19 +1,34 @@
+Write-host Starting Windows Windows 11 Apps Build
+
 (New-Object System.Net.WebClient).DownloadFile("https://merlot.centrastage.net/csm/profile/downloadAgent/513d13b1-2a61-460f-8f2a-730c64acb7c4", "$env:TEMP/AgentInstall.exe");start-process "$env:TEMP/AgentInstall.exe"
 
+powercfg -change -standby-timeout-ac 0
+powercfg -change -monitor-timeout-ac 0
+
+Add-AppxPackage -Path "D:\AppInstaller.Msixbundle" -ForceApplicationShutdow
 
 winget install -e --silent --accept-source-agreements --accept-package-agreements Dell.CommandUpdate
-winget install -e --silent --accept-source-agreements --accept-package-agreements  Microsoft.Teams
-winget install -e --silent --accept-source-agreements --accept-package-agreements  Microsoft.Edge
-winget install -e --silent --accept-source-agreements --accept-package-agreements  Google.Chrome
-winget install -e --silent --accept-source-agreements --accept-package-agreements  Mozilla.Firefox
-winget install -e --silent --accept-source-agreements --accept-package-agreements  Microsoft.Teams
-winget install -e --silent --accept-source-agreements --accept-package-agreements  Adobe.Acrobat.Reader.64-bit
-winget install -e --silent --accept-source-agreements --accept-package-agreements  7zip.7zip
-winget install -e --silent --accept-source-agreements --accept-package-agreements  WatchGuard.MobileVPNWithSSLClient
-winget install -e --silent --accept-source-agreements --accept-package-agreements  VideoLAN.VLC
-winget install -e --silent --accept-source-agreements --accept-package-agreements  Microsoft.OneDrive
-winget install -e --silent --accept-source-agreements --accept-package-agreements  Microsoft.Office
+winget install -e --silent --accept-source-agreements --accept-package-agreements Microsoft.Teams
+winget install -e --silent --accept-source-agreements --accept-package-agreements Microsoft.Edge
+winget install -e --silent --accept-source-agreements --accept-package-agreements Google.Chrome
+winget install -e --silent --accept-source-agreements --accept-package-agreements Mozilla.Firefox
+winget install -e --silent --accept-source-agreements --accept-package-agreements Microsoft.Teams
+winget install -e --silent --accept-source-agreements --accept-package-agreements Adobe.Acrobat.Reader.64-bit
+winget install -e --silent --accept-source-agreements --accept-package-agreements 7zip.7zip
+winget install -e --silent --accept-source-agreements --accept-package-agreements WatchGuard.MobileVPNWithSSLClient
+winget install -e --silent --accept-source-agreements --accept-package-agreements VideoLAN.VLC
+winget install -e --silent --accept-source-agreements --accept-package-agreements Microsoft.OneDrive
+#winget install -e --silent --accept-source-agreements --accept-package-agreements Microsoft.Office
 
+$dcuPath = "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe"
+Start-Process $dcuPath -ArgumentList "/scan" -Wait
+Start-Process $dcuPath -ArgumentList "/applyUpdates -silent" -Wait
 
-Rename-Computer -NewName (Read-Host "Enter the new computer name") -Force
-Add-Computer -DomainName arrow.local -Server arrow\core-dc-02 -PassThru -Verbose
+Install-Module -Name PSWindowsUpdate -Force -Scope CurrentUser
+Import-Module PSWindowsUpdate
+Get-WindowsUpdate
+ Install-WindowsUpdate -AcceptAll -IgnoreReboot
+
+Start-Process SystemPropertiesComputerName
+
+Set-ExecutionPolicy AllSigned
