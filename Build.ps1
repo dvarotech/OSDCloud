6b43b8d80dfe27ef109559af6c5b468d9c20c927
dvarotech <david.vaughan@aro.tech>
2025-04-11 18:00:41 +0100

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -5,7 +5,7 @@ Write-host Starting Windows Windows 11 Apps Build
 powercfg -change -standby-timeout-ac 0
 powercfg -change -monitor-timeout-ac 0
 
-Get-AppxPackage -AllUsers | Foreach {Try {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml" -ErrorAction Stop} Catch {Write-Host "Failed to update $($_.Name): $_"}}
+#Get-AppxPackage -AllUsers | Foreach {Try {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml" -ErrorAction Stop} Catch {Write-Host "Failed to update $($_.Name): $_"}}
 
 winget upgrade --all -e --silent --accept-source-agreements --accept-package-agreements
 winget install -e --silent --accept-source-agreements --accept-package-agreements Dell.CommandUpdate
