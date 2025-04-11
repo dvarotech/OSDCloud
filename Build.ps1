bd2de15a32305ca7590bf7f80e963d63d8fb4bc8
dvarotech <david.vaughan@aro.tech>
2025-04-11 16:19:01 +0100

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,21 +1,21 @@
 Write-host Starting Windows Windows 11 Apps Build
 
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+#[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install office365business --version 18526.20146 -y
+#choco install office365business --version 18526.20146 -y
 
 (New-Object System.Net.WebClient).DownloadFile("https://merlot.centrastage.net/csm/profile/downloadAgent/513d13b1-2a61-460f-8f2a-730c64acb7c4", "$env:TEMP/AgentInstall.exe");start-process "$env:TEMP/AgentInstall.exe"
 
 powercfg -change -standby-timeout-ac 0
 powercfg -change -monitor-timeout-ac 0
 
-#winget upgrade --all --include-unknown --accept-source-agreements --accept-package-agreements
+winget upgrade --all --include-unknown --accept-source-agreements --accept-package-agreements
 
 #Get-AppxPackage *AppInstaller* | Remove-AppxPackage
 #Add-AppxPackage -Path "D:\AppInstaller.Msixbundle" -ForceApplicationShutdow 
-Add-AppxPackage -AppInstallerFile "D:\AppInstaller.Msixbundle"
+#Add-AppxPackage -AppInstallerFile "D:\AppInstaller.Msixbundle"
 
-#winget install -e --silent --accept-source-agreements --accept-package-agreements Dell.CommandUpdate
+winget install -e --silent --accept-source-agreements --accept-package-agreements Dell.CommandUpdate
 #winget install -e --silent --accept-source-agreements --accept-package-agreements Microsoft.Teams
 #winget install -e --silent --accept-source-agreements --accept-package-agreements Microsoft.Edge
 #winget install -e --silent --accept-source-agreements --accept-package-agreements Google.Chrome
@@ -35,6 +35,6 @@ Start-Process $dcuPath -ArgumentList "/applyUpdates -silent" -Wait
 Install-Module -Name PSWindowsUpdate -Force -Scope CurrentUser
 Import-Module PSWindowsUpdate
 Get-WindowsUpdate
- Install-WindowsUpdate -AcceptAll -IgnoreReboot
+Install-WindowsUpdate -AcceptAll -IgnoreReboot
 
 Start-Process SystemPropertiesComputerName
