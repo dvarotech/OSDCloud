ae99a4c37e6110e939351a996bb6f82814c52f16
dvarotech <david.vaughan@aro.tech>
2025-04-13 15:19:13 +0100

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -4,12 +4,8 @@ Write-host Starting Windows Windows 11 Apps Build v4
 powercfg -change -standby-timeout-ac 0
 powercfg -change -monitor-timeout-ac 0
 
-Install-Module -Name PSWindowsUpdate -Force -Scope CurrentUser
-Import-Module PSWindowsUpdate
-Get-WindowsUpdate
-Install-WindowsUpdate -AcceptAll -IgnoreReboot
 
-winget install microsoft.office
+
 
 #installs datto
 (New-Object System.Net.WebClient).DownloadFile("https://merlot.centrastage.net/csm/profile/downloadAgent/513d13b1-2a61-460f-8f2a-730c64acb7c4", "$env:TEMP/AgentInstall.exe");start-process "$env:TEMP/AgentInstall.exe"
@@ -30,6 +26,11 @@ choco install vlc -y
 choco install onedrive -y
 
 choco upgrade all -y
+
+Install-Module -Name PSWindowsUpdate -Force -Scope CurrentUser
+Import-Module PSWindowsUpdate
+Get-WindowsUpdate
+Install-WindowsUpdate -AcceptAll -IgnoreReboot
 
 $dcuPath = "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe"
 Start-Process $dcuPath -ArgumentList "/scan" -Wait
