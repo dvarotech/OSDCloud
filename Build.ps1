7ba29f333ca34b57173121b0bc7f847a16ce2354
dvarotech <david.vaughan@aro.tech>
2025-04-14 08:20:58 +0100

    Update Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,4 +1,4 @@
-Write-host Starting Windows Windows 11 Apps Build v6
+Write-host Starting Windows Windows 11 Apps Build v7
 
 #Set power plan to always on
 powercfg -change -standby-timeout-ac 0
@@ -6,13 +6,15 @@ powercfg -change -monitor-timeout-ac 0
 
 
 
-
+Write-host Installing Datto Agent
 #installs datto
 (New-Object System.Net.WebClient).DownloadFile("https://merlot.centrastage.net/csm/profile/downloadAgent/513d13b1-2a61-460f-8f2a-730c64acb7c4", "$env:TEMP/AgentInstall.exe");start-process "$env:TEMP/AgentInstall.exe"
 
+Write-host Installing chocolatey and apps
 #installs chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+#installs apps
 choco install microsoft-office-deployment /64 -y
 choco install dellcommandupdate -y
 choco install office365proplus -y
@@ -25,18 +27,16 @@ choco install 7zip -y
 choco install vlc -y
 choco install onedrive -y
 
-choco upgrade all -y
 
+Write-host Download and installl Windows updates
 Install-Module -Name PSWindowsUpdate -Force -Scope CurrentUser
 Import-Module PSWindowsUpdate
 Get-WindowsUpdate
 Install-WindowsUpdate -AcceptAll -IgnoreReboot
 
+Write-host Check for Dell updates
 $dcuPath = "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe"
 Start-Process $dcuPath -ArgumentList "/scan" -Wait
 Start-Process $dcuPath -ArgumentList "/applyUpdates -silent" -Wait
-
-
-
 
 Start-Process SystemPropertiesComputerName
