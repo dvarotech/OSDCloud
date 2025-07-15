545d23d4a8d64fe722a7d1d926c39b6739750316
dvarotech <david.vaughan@aro.tech>
2025-07-15 13:14:08 +0100

    Update StartOSD.ps1

--- StartOSD.ps1
+++ StartOSD.ps1
@@ -1,3 +1,2 @@
 Write-Host "Starting ARO Internal IT Windows 11 24h2 - 15-07-2025." -ForegroundColor Green
-start-sleep 10
 Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Pro -OSLanguage en-gb -OSActivation Retail -Firmware -ZTI -Restart
