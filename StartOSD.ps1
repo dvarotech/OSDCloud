6f737280df12cb132ce9390ccc5f67ab0f7cc6c4
dvarotech <david.vaughan@aro.tech>
2025-10-17 16:08:44 +0100

    Update StartOSD.ps1

--- StartOSD.ps1
+++ StartOSD.ps1
@@ -1,2 +1,5 @@
 Write-Host "Starting ARO Internal IT Windows 11 25h2 - 13-10-2025." -ForegroundColor Green
-Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 25H2 -OSEdition Pro -OSLanguage en-gb -OSActivation Retail -Firmware -ZTI -Restart
+Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 25H2 -OSEdition Pro -OSLanguage en-gb -OSActivation Retail -Firmware -ZTI
+Write-Host "Downloading Post Install Script - 17-10-2025." -ForegroundColor Green
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dvarotech/OSDCloud/main/Build.ps1" -OutFile "C:\Build.ps1"
+
