112437e0986a6374bfea3d11e4d4e06e5326178e
dvarotech <david.vaughan@aro.tech>
2025-10-20 10:40:01 +0100

    Update StartOSD.ps1

--- StartOSD.ps1
+++ StartOSD.ps1
@@ -1,11 +1,11 @@
 Write-Host "Starting ARO Internal IT Windows 11 25h2 - 13-10-2025." -ForegroundColor Green
 Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 25H2 -OSEdition Pro -OSLanguage en-gb -OSActivation Retail -Firmware -ZTI
 Write-Host "Downloading Post Install Script - 20-10-2025." -ForegroundColor Green
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dvarotech/OSDCloud/main/Build.ps1" -OutFile "C:\Build.ps1"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dvarotech/OSDCloud/main/Build.ps1" -OutFile "D:\Build.ps1"
+Invoke-WebRequest -Uri "http://159.65.21.143/Build/Build.ps1" -OutFile "C:\Build.ps1"
+Invoke-WebRequest -Uri "http://159.65.21.143/Build/Build.ps1" -OutFile "D:\Build.ps1"
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dvarotech/OSDCloud/refs/heads/main/Join.ps1" -OutFile "C:\Join.ps1"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dvarotech/OSDCloud/refs/heads/main/Join.ps1" -OutFile "D:\Join.ps1"
+Invoke-WebRequest -Uri "http://159.65.21.143/Build/Join.ps1" -OutFile "C:\Join.ps1"
+Invoke-WebRequest -Uri "http://159.65.21.143/Build/Join.ps1" -OutFile "D:\Join.ps1"
 
 Write-Host "Rebooting." -ForegroundColor Green
 Start-Sleep 5
