4f7f54809de6c43e736a44f5fc4ab2921baeac8e
dvarotech <david.vaughan@aro.tech>
2025-10-20 10:51:33 +0100

    Update Join.ps1

--- Join.ps1
+++ Join.ps1
@@ -1,12 +1,1 @@
-$Domain = 'arrow.local'
-$Credential = Get-Credential
 
-Add-Computer -DomainName $Domain `
-    -OUPath 'OU=Haydock,OU=Arrow-EndPoints,DC=arrow,DC=local' `
-    -Credential $Credential -Force
-
-gpupdate
-gpupdate /sync
-
-start-sleep 60
-Restart-Computer
