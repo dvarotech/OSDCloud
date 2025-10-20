d6d68da1faf8cfa2cf4e43a47fb214d829a0823c
dvarotech <david.vaughan@aro.tech>
2025-10-20 09:33:22 +0100

    Update StartOSD.ps1

--- StartOSD.ps1
+++ StartOSD.ps1
@@ -3,6 +3,10 @@ Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 25H2 -OSEdition Pro -OSLanguage 
 Write-Host "Downloading Post Install Script - 17-10-2025." -ForegroundColor Green
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dvarotech/OSDCloud/main/Build.ps1" -OutFile "C:\Build.ps1"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dvarotech/OSDCloud/main/Build.ps1" -OutFile "D:\Build.ps1"
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dvarotech/OSDCloud/refs/heads/main/Join.ps1" -OutFile "C:\Build.ps1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/dvarotech/OSDCloud/refs/heads/main/Join.ps1" -OutFile "D:\Build.ps1"
+
 Write-Host "Rebooting." -ForegroundColor Green
 Start-Sleep 5
 Restart-Computer
